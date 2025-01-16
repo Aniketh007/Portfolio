ba469842b740b8932417a4a197194489ef92d2fb
Aniketh Reddy <96186902+Aniketh007@users.noreply.github.com>
2025-01-16 23:35:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+'use strict';
+
+
+
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-selecct-value]");
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+select.addEventListener("click", function () { elementToggleFunc(this); });
+
+// add event in all select items
+for (let i = 0; i < selectItems.length; i++) {
+  selectItems[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    elementToggleFunc(select);
+    filterFunc(selectedValue);
+
+  });
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+  for (let i = 0; i < filterItems.length; i++) {
+    console.log(filterItems[i]);
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+
+  }
+
+}
+
+// add event in all filter button items for large screen
+let lastClickedBtn = filterBtn[0];
+
+for (let i = 0; i < filterBtn.length; i++) {
+
+  filterBtn[i].addEventListener("click", function () {
+
+    let selectedValue = this.innerText.toLowerCase();
+    selectValue.innerText = this.innerText;
+    filterFunc(selectedValue);
+
+    lastClickedBtn.classList.remove("active");
+    this.classList.add("active");
+    lastClickedBtn = this;
+
+  });
+
+}
+
+
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+
+  });
+}
+
+
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+
+    for (let i = 0; i < pages.length; i++) {
+      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
+        pages[i].classList.add("active");
+        navigationLinks[i].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[i].classList.remove("active");
+        navigationLinks[i].classList.remove("active");
+      }
+    }
+
+  });
+}
+
+function openResume(event) {
+  event.preventDefault(); // Prevent the default action (e.g., page reload)
+  window.open('https://drive.google.com/file/d/1gwEm4z9f3y-_F2e6HG5aNFwqOnsbkXUC/view?usp=drive_link', '_blank');
+}
+
+function toggleSidebar() {
+  const sidebarContent = document.getElementById('sidebar-content');
+  const button = document.querySelector('[data-sidebar-btn]');
+  
+  if (sidebarContent.classList.contains('hidden')) {
+    sidebarContent.classList.remove('hidden');
+    button.querySelector('span').textContent = 'Hide Contact';
+    button.querySelector('ion-icon').setAttribute('name', 'close-outline');
+  } else {
+    sidebarContent.classList.add('hidden');
+    button.querySelector('span').textContent = 'Show Contact';
+    button.querySelector('ion-icon').setAttribute('name', 'menu-outline');
+  }
+}
+
+document.querySelector('[data-sidebar-btn]').addEventListener('click', toggleSidebar);
